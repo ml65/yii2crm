c440bce25b444080632021e86a3f0c4584eeef86
ml65 <ml65@rambler.ru>
2024-09-29 22:20:50 +0000

    add_migration

---
+++ console/migrations/m240928_141100_create_bid_table.php
@@ -0,0 +1,32 @@
+<?php
+use yii\db\Migration;
+
+class m240928_141100_create_bid_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%bid}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'title' => $this->string(255)->notNull(),
+            'product_name' => $this->string()->notNull(),
+            'phone' => $this->string(11),
+            'comment' => $this->text(),
+            'price' => $this->float()->notNull(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(0),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%bid}}');
+    }
+}
